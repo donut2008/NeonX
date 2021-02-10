fddbcb65a3225df4f98555c95afaa784ad83bebd
Jakub <66490771+alur2020@users.noreply.github.com>
2021-02-10 14:21:22 +0100

    update 2
    added apps
    fixed search
    fixed lists

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// add new todo when enter is pressed. code by emeraldvideos2
 document.getElementById("myInput").addEventListener("keyup", (event) => {
   if (event.keyCode === 13) {
     event.preventDefault();
@@ -12,6 +13,98 @@ list.addEventListener('click', function(ev) {
     ev.target.classList.toggle('checked');
   }
 }, false);
+
+
+//window code
+// Make the DIV element draggable:
+dragElement(document.getElementById("window"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+
+//window code
+// Make the DIV element draggable:
+dragElement(document.getElementById("windowexceed"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
@@ -78,6 +171,24 @@ function dateTime() {
   var year = dateObj.getUTCFullYear();
   document.getElementById('date').innerHTML =
   day + "/" + month + "/" + year;
+}
+function exceedapp(){
+    document.getElementById("windowexceed").style.display = "block";
+    document.getElementById("searchoverlay").style.display = "none";
+    document.getElementById("calendaroverlay").style.display = "none";
+    document.getElementById("launcher").style.display = "none";
+}
+function openapp(){
+  document.getElementById("window").style.display = "block";
+  document.getElementById("searchoverlay").style.display = "none";
+  document.getElementById("calendaroverlay").style.display = "none";
+  document.getElementById("launcher").style.display = "none";
+}
+function closeexceed(){
+  document.getElementById("windowexceed").style.display = "none";
+}
+function exit(){
+    document.getElementById("window").style.display = "none";
 }
 function on() {
     document.getElementById("launcher").style.display = "block";
